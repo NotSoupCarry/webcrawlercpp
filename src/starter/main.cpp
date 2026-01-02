ca137eb0c7cd899d3420569655965c8e3ca142a8
Soup <giuseppeforce36@gmail.com>
2026-01-02 17:48:44 +0100

    Update main.cpp

--- src/starter/main.cpp
+++ src/starter/main.cpp
@@ -9,15 +9,20 @@ using namespace std;
 
 int main() {
     cout << "=== WebCrawler ===" << endl;
-    string startUrl = "https://www.archlinux.org/";
+    string startUrl = "https://www.youtube.com/";
     // string startUrl;
     // cout<< "enter the starting url: "; cin >> startUrl;
+    // int maxDepth;
+    // cout<< "enter the maxDepth: "; cin >> maxDepth;
+    // int maxLinks;
+    // cout<< "enter the Links per page limit: "; cin >> maxLinks;
 
     int maxDepth = 2;
     int maxLinks = 2;
 
     cout << "  URL: " << startUrl << endl;
     cout << "  Max Depth: " << maxDepth << endl << endl;
+    cout << "  Max Links per Page: " << maxLinks << endl << endl;
 
     WebCrawler crawler(maxDepth, maxLinks);
     crawler.crawl(startUrl);
